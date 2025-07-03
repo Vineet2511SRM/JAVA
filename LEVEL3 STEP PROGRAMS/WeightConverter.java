22fa38255c3fb989ce55c31358e92265d7f6d143
Vineet Seth <vs4622@srmist.edu.in>
2025-07-03 23:39:57 +0530

    Update WeightConverter.java

--- LEVEL3 STEP PROGRAMS/WeightConverter.java
+++ LEVEL3 STEP PROGRAMS/WeightConverter.java
@@ -2,15 +2,15 @@ import java.util.*;
 
 public class WeightConverter {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner obj = new Scanner(System.in);
 
         System.out.print("Enter weight in pounds: ");
-        double weightInPounds = sc.nextDouble();
+        double weightInPounds = obj.nextDouble();
 
         double weightInKg = weightInPounds / 2.2;
 
         System.out.println("The weight of the person in pounds is " + weightInPounds + " and in kilograms is " + weightInKg);
 
-        sc.close();
+        obj.close();
     }
 }
